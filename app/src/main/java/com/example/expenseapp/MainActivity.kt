52e6c94da9cb9f6d47f3a604f78588402f479670
aprameyaneopane <aprameyaneopane@gmail.com>
2023-10-10 14:38:17 +0545

    search and fav icons

--- app/src/main/java/com/example/expenseapp/MainActivity.kt
+++ app/src/main/java/com/example/expenseapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.expenseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.expenseapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolBar)
+        supportActionBar?.title = "Transactions"
+        window.statusBarColor = getColor(R.color.orange)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu,menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
